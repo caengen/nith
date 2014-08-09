9aa2dbe003b730edde69674c89143f10b83b32c1
C. Engen <chris.a.engen@gmail.com>
2014-08-09 15:02:18 +0200

    Changed content in cart view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
 	default_scope order: 'title'
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 	# Validations
 	validates :title, uniqueness: true, length: { minimum: 10 }
@@ -9,4 +11,15 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	private 
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty? 
+			return true 
+		else 
+			errors.add(:base, 'Line Items present')
+			return false 
+		end
+	end
 end
