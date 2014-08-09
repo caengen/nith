a97b273fb363bfcc8a2cb476f33efe79ba1de735
C. Engen <chris.a.engen@gmail.com>
2014-08-09 19:49:26 +0200

    Combined current line_items in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809160644) do
+ActiveRecord::Schema.define(version: 20140809174140) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
