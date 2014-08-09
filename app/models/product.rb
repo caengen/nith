24f98479d949b3aca633e1f349a19518d4c752de
C. Engen <chris.a.engen@gmail.com>
2014-08-09 11:18:08 +0200

    Added new index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ActiveRecord::Base
-	validates :title, uniqueness: true
+	default_scope order: 'title'
+
+	# Validations
+	validates :title, uniqueness: true, length: { minimum: 10 }
 	validates :title, :image_url, :description, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :image_url, format: {
-		with: %r{\.(gif|jpg|png)}i,
+		with: %r{\.(gif|jpg|png)\z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
 end
